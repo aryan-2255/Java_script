5fa8a92cd55972dfc7a5158f93224e363142996b
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2025-10-06 16:30:13 +0530

    JS

---
+++ sept_25_string.js
@@ -0,0 +1,72 @@
+// console.log("Hello \nWorld");
+// console.log('Hello World')
+
+// let name = "aryan"
+// let age = 19
+
+// console.log("hello my name is " +name);
+// console.log(hello my name is ${name}  my age is ${age});
+
+
+// let str="Hello Aryan"
+// console.log(str.length);
+// let newstr=(str.trim());
+// console.log(newstr[2],str[0]);
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+
+
+// let str= "hello shinchane"
+// console.log(str.substring(-2,4))       // substring do not take negative value else it start from zero(0)
+
+
+
+
+
+// slice
+
+// let str = "helloworld";
+// console.log(str.slice(1,-3))
+
+
+
+//replace
+
+// let str = "i am aryan"
+// console.log(str.replace("aryan", "kunal"))
+
+
+
+// split
+// let str= "i am aryan"
+// console.log(str.split(" "))
+
+
+
+
+// operators                         
+                                     // Arithmetic Operators
+                                     // Assignment Operators
+                                     // Comparison Operators
+                                     // Logical Operators =     &&	logical and
+                                     //                         ||	logical or
+                                     //                          !	 logical not
+
+                                     // And more ...
+
+
+// let x = 30
+// x = x+30
+// x = x*30
+// x = x/30
+// x = x%30
+// x = x**30
+// console.log(x);
+
+// {
+// let y = 20
+// let y = 20++
+// let newy = ++y
+// console.log(y,newy)
+// }
