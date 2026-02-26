4636e7e9a413a0eb9ed50a12032b6d0740f14ad0
aryanmatrixx-oss <aryan.matrixx@gmail.com>
2026-02-26 23:22:20 +0530

    Update files

--- Js end sem 2/script.js
+++ Js end sem 2/script.js
@@ -1,9 +1,14 @@
 function test(callback){
-    fetch('api')
-    .then(response => response.json())
-    .then(json => callback(json))
+    fetch('https://api.github.com/zen')
+    .then(response => response.text())
+    .then(text => callback(text))
     .catch(error => console.error('error ha catch work kar ra ha', error))
+    callback("data nahi aaya");
 }
+function hi(){
 test(function(data){
+    document.getElementById('p').innerHTML = data;
     console.log("api data")
-})
+    
+})}
+hi();
